789faf9c380ba59c33f7a4001bdcd3d58847335d
Your Name <you@example.com>
2016-04-03 12:58:51 +0100

    Now only controls a single GPIO pin for the sake of simplicity.

--- lib/rpi_pwm.rb
+++ lib/rpi_pwm.rb
@@ -7,148 +7,104 @@ require 'rpi_gpio'
 
 class RPiPwm
 
+  HIGH = true
+  LOW = false
 
-  class PinX
-
-    HIGH = true
-    LOW = false
-
-    attr_accessor :duty_cycle, :freq
+  attr_accessor :duty_cycle, :freq
 
 
-    def initialize(pin_num, duty_cycle: 100, freq: 100)
+  def initialize(pin_num, duty_cycle: 100, freq: 100)
 
-      @id, @duty_cycle, @freq = pin_num, duty_cycle, freq
+    @id, @duty_cycle, @freq = pin_num, duty_cycle, freq
 
-      RPi::GPIO.set_numbering :bcm
-      RPi::GPIO.setup pin_num, :as => :output
+    RPi::GPIO.set_numbering :bcm
+    RPi::GPIO.setup pin_num, :as => :output
 
-      @pwm = RPi::GPIO::PWM.new(pin_num, freq)
-      @pwm.frequency = 40
+    @pwm = RPi::GPIO::PWM.new(pin_num, freq)
 
-      @on, @blinking = false, false
+    @on, @blinking = false, false
 
-      at_exit { RPi::GPIO.clean_up pin_num}
+    at_exit { RPi::GPIO.clean_up pin_num}
 
-    end
+  end
 
-    def duty_cycle=(val)
-      @duty_cycle = val
-      @pwm.duty_cycle = val
-    end
+  def duty_cycle=(val)
+    @duty_cycle = val
+    @pwm.duty_cycle = val
+  end
 
-    def on(durationx=nil, duration: nil)
+  def on(durationx=nil, duration: nil)
 
-      set_pin HIGH
-      @on = true      
-      duration ||=  durationx
-      
-      @off_thread.exit if @off_thread
-      @on_thread = Thread.new {(sleep duration; self.off()) } if duration
-
-    end
-
-    def off(durationx=nil, duration: nil)
-
-      set_pin LOW
-      return if @internal_call
-      
-      @on, @blinking = false, false      
-      duration ||=  durationx
-      
-      @on_thread.exit if @on_thread
-      @off_thread = Thread.new { (sleep duration; self.on()) } if duration
-
-    end
+    set_pin HIGH
+    @on = true      
+    duration ||=  durationx
     
-    alias stop off        
-    alias start on
+    @off_thread.exit if @off_thread
+    @on_thread = Thread.new {(sleep duration; self.off()) } if duration
 
-    def blink(seconds=0.5, duration: nil)
+  end
 
-      @blinking = true
-      t2 = Time.now + duration if duration
+  def off(durationx=nil, duration: nil)
 
-      Thread.new do
-        while @blinking do
+    set_pin LOW
+    return if @internal_call
+    
+    @on, @blinking = false, false      
+    duration ||=  durationx
+    
+    @on_thread.exit if @on_thread
+    @off_thread = Thread.new { (sleep duration; self.on()) } if duration
 
-          set_pin HIGH
-          sleep seconds / 2.0
-          break if !@blinking
-          sleep seconds / 2.0 
-          break if !@blinking
-          
-          set_pin LOW;
-          sleep seconds / 2.0
-          break if !@blinking
-          sleep seconds / 2.0
+  end
+  
+  alias stop off        
+  alias start on
 
-          break if !@blinking
-          
-          self.off if duration and Time.now >= t2
-        end
+  def blink(seconds=0.5, duration: nil)
+
+    @blinking = true
+    t2 = Time.now + duration if duration
+
+    Thread.new do
+      while @blinking do
+
+        set_pin HIGH
+        sleep seconds / 2.0
+        break if !@blinking
+        sleep seconds / 2.0 
+        break if !@blinking
         
-      end
-    end
-    
-    alias oscillate blink
+        set_pin LOW;
+        sleep seconds / 2.0
+        break if !@blinking
+        sleep seconds / 2.0
 
-    def on?()  @on  end
-    def off?() !@on end
-
-    # set val with 0 (off) or 1 (on)
-    #
-    def set_pin(val)
-
-      if val then
-        @pwm.start @duty_cycle
-      else
-        @pwm.stop
-      end
+        break if !@blinking
         
-    end
-    
-    def to_s()
-      @id
+        self.off if duration and Time.now >= t2
+      end
+      
     end
   end
   
-  class Void
-    def on(duration=nil)               end
-    def off()                          end
-    def blink(seconds=0, duration=nil) end
-    alias stop off
-  end  
+  alias oscillate blink
 
+  def on?()  @on  end
+  def off?() !@on end
 
-  def initialize(x=[], duty_cycle: 100, freq: 100)
-    
-    a = case x
-    when Fixnum
-      [x]
-    when String
-      [x]
-    when Array
-      x
+  # set val with 0 (off) or 1 (on)
+  #
+  def set_pin(val)
+
+    if val then
+      @pwm.start @duty_cycle
+    else
+      @pwm.stop
     end
-
-    RPi::GPIO.reset
-    
-    @pins = a.map {|pin| PinX.new pin, duty_cycle: duty_cycle, freq: freq }
-    
-    def @pins.[](i)
-
-      if i.respond_to? :to_i and i.to_i >= self.length then
-        puts "RPiPwm warning: PinX instance #{i.inspect} not found"
-        Void.new
-      else
-        super(i)
-      end 
-    end    
       
   end
-
-  def pin()   @pins.first  end
-  def pins()  @pins        end
-
+  
+  def to_s()
+    @id
+  end
 end
--- rpi_pwm.gemspec
+++ rpi_pwm.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'rpi_pwm'
-  s.version = '0.1.1'
+  s.version = '0.2.0'
   s.summary = 'A wrapper of the rpi_gpio gem with similar function to the rpi gem, but features PWM.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/rpi_pwm.rb']
